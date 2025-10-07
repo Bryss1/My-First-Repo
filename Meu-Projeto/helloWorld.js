02d855a8bfcc3b69d3afcb3429ae7525236c2884
Brunobvm1 <brunobvm1@hotmail.com>
2025-10-07 13:06:35 -0300

    transferido arquivo editado para a pasta principal

---
+++ Meu-Projeto/helloWorld.js
@@ -0,0 +1,11 @@
+console.log("Olá, Mundo");
+const mensagemOlaMundoEstilizada = [
+    mensagemOlaMundoEstilizada.caixaDeFundo = ();
+mensagemOlaMundoEstilizada.corDeFundo = ();
+mensagemOlaMundoEstilizada.borda = ();
+mensagemOlaMundoEstilizada.transparencia = ();
+mensagemOlaMundoEstilizada.texto = ();
+mensagemOlaMundoEstilizada.textura = ();
+
+
+];
